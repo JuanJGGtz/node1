d2f044aa90e8b32f541af23f2b63f2b2770de281
JuanJGGtz <66813179+JuanJGGtz@users.noreply.github.com>
2023-11-01 08:30:35 -0600

    Iniciando con node node init -y writeFile

---
+++ app.js
@@ -0,0 +1,13 @@
+import { writeFile } from 'node:fs';
+
+let base = 5;
+let salida = "";
+for (let index = 1; index <= 10; index++) {
+    salida += `${base} * ${index} : ${base * index}\n`;
+}
+
+writeFile('message.txt', salida, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
+
