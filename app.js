50f02ba67b9858785334a45cae1fd351b4480679
JuanJGGtz <66813179+JuanJGGtz@users.noreply.github.com>
2023-11-02 07:44:18 -0600

    import

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-import { writeFile } from 'node:fs';
+import {crearArchivoTablaMultiplicar} from './helpers/multiplicar.js';
 
-let base = 5;
-let salida = "";
-for (let index = 1; index <= 10; index++) {
-    salida += `${base} * ${index} : ${base * index}\n`;
-}
-
-writeFile('message.txt', salida, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-});
-
+crearArchivoTablaMultiplicar();
